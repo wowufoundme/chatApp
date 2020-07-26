dafdeed6d6bd069b8be741c3c63450e01e56d286
Shubhkirti Sharma <shubhkirti.work@gmail.com>
2020-07-26 23:47:21 +0530

    add socket connect and disconnect for user

--- server/index.js
+++ server/index.js
@@ -10,6 +10,15 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+io.on('connection', (socket) => {
+  console.log('We have a new connection on socket', socket);
+
+  socket.on('disconnect', ()+> {
+    console.log('User has left')
+  })
+
+})
+
 app.use(router);
 
 server.listen(PORT, () => console.log(`server has started on port ${PORT}`))
