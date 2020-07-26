2fc323f6d229c9398e5b4c2c86909985f920e5ea
Shubhkirti Sharma <shubhkirti.work@gmail.com>
2020-07-27 03:17:36 +0530

    Change source directories for components

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,8 @@ import {
   Route
 } from 'react-router-dom';
 
-import Join from './components/Join';
-import Chat from './components/Chat';
+import Join from './components/Join/Join.component';
+import Chat from './components/Chat/Chat.component';
 
 const App = () => {
   return (
