ee6b63e7e75f7ebe61d55be995de002c8eb75530
Shubhkirti Sharma <shubhkirti.work@gmail.com>
2020-07-27 23:20:05 +0530

    Add Chatbox header component and styles

---
+++ client/src/components/Header/Header.component.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Header.styles.css';
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Chat Header</h1>
+    </div>
+  )
+}
+
+export default Header;
