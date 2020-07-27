e1ceca0836a6a63d53821d594c7f366eb413e069
Shubhkirti Sharma <shubhkirti.work@gmail.com>
2020-07-27 19:58:21 +0530

    Setup state and hooks in Chat

--- client/src/components/Chat/Chat.component.js
+++ client/src/components/Chat/Chat.component.js
@@ -1,7 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import queryString from 'query-string';
+import io from 'socket.io-client';
+
+let socket;
 
 const Chat = props => {
+  const { location } = props;
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+    setName(name);
+    setRoom(room);
+  })
+
   return (
     <div>
       <h1>Hello world Chat</h1>
