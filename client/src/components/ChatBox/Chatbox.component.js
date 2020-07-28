00d07ce4c7a9f6da1704fe8fd750b6d85e1edca7
Shubhkirti Sharma <shubhkirti.work@gmail.com>
2020-07-28 21:02:44 +0530

    Include MessageBox and Input component

--- client/src/components/ChatBox/Chatbox.component.js
+++ client/src/components/ChatBox/Chatbox.component.js
@@ -3,12 +3,33 @@ import React from 'react';
 import './Chatbox.styles.css'
 
 import Header from '../Header/Header.component'
+import MessagesBox from '../MessagesBox/MessagesBox.component';
+import Input from '../Input/Input.component';
 
 const Chatbox = props => {
+
+  const {
+    name,
+    chatRoom,
+    message,
+    setMessage,
+    sendMessage
+  } = props;
+
   return (
     <div className='mainContainer'>
       <div className='chatBoxContainer'>
-        <Header room={props.chatRoom}/>
+        <Header room={chatRoom}/>
+        <MessagesBox />
+        <Input message={message} setMessage={setMessage} sendMessage={sendMessage}/>
+        <input
+          value={message}
+          type='text'
+          placeholder='Type message...'
+          onChange={(event) => setMessage(event.target.value)}
+          onKeyPress={(event) => event.key === 'Enter' ? sendMessage(event) : null }
+          className='sendMessageInput'
+        />
       </div>
     </div>
   )
