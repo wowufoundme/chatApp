282f01a23c9483926e47d41a52ecc3745abc4fcd
Shubhkirti Sharma <shubhkirti.work@gmail.com>
2020-07-29 00:56:16 +0530

    Show name in message box of the user who sent the message

--- client/src/components/MessagesBox/MessagesBox.component.js
+++ client/src/components/MessagesBox/MessagesBox.component.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ScrollToBottom from 'react-scroll-to-bottom';
 
+import Message from '../Message/Message.component';
+
 import './MessagesBox.styles.css';
 
 const MessagesBox = props => {
+
+  const { messages, name } = props;
+
   return (
     <ScrollToBottom>
-      
+      {messages.map((message, i) => <div key={i}><Message message={message} name={name}/></div>)}
     </ScrollToBottom>
   )
 }
