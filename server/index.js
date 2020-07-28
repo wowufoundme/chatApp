65966198233c7f70fd51f5532761bd71f8efa8bb
Shubhkirti Sharma <shubhkirti.work@gmail.com>
2020-07-29 02:27:40 +0530

    Fix callback to empty input string

--- server/index.js
+++ server/index.js
@@ -29,10 +29,15 @@ io.on('connection', (socket) => {
   socket.on('sendMessage', (message, callback) => {
     const user = getUser(socket.id)
     io.to(user.room).emit('message', { user: user.name, text: message })
+    callback();
   })
 
   socket.on('disconnect', () => {
-    console.log('User has left')
+    const user = removeUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit('message', { user: 'admin', text: `${user.name} has left the chat` });
+    }
   })
 
 })
