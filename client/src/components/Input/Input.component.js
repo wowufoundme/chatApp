3bdd4094efd6dbf754a2004902659c0b282ca5d0
Shubhkirti Sharma <shubhkirti.work@gmail.com>
2020-07-28 21:59:51 +0530

    Fix more styling issues and layout

--- client/src/components/Input/Input.component.js
+++ client/src/components/Input/Input.component.js
@@ -8,21 +8,23 @@ const Input = props => {
 
   return (
     <div className='sendMessageContainer'>
-      <input
-        value={message}
-        type='text'
-        placeholder='Type message...'
-        onChange={(event) => setMessage(event.target.value)}
-        onKeyPress={(event) => event.key === 'Enter' ? sendMessage(event) : null }
-        className='sendMessageInput'
-      />
-      <button
-        type='submit'
-        onClick={(event) => sendMessage(event)}
-        className='sendButton'
-      >
-        Send
-      </button>
+      <div className='inputHolder'>
+        <input
+          value={message}
+          type='textarea'
+          placeholder='Type message...'
+          onChange={(event) => setMessage(event.target.value)}
+          onKeyPress={(event) => event.key === 'Enter' ? sendMessage(event) : null }
+          className='sendMessageInput'
+        />
+        <button
+          type='submit'
+          onClick={(event) => sendMessage(event)}
+          className='sendButton'
+        >
+          Send
+        </button>
+      </div>
     </div>
   )
 }
