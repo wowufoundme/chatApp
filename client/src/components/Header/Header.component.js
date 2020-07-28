43ab91e39f9bfa2c9311f0fd475af0beaa60538a
Shubhkirti Sharma <shubhkirti.work@gmail.com>
2020-07-28 21:54:40 +0530

    Switch from H3 to span

--- client/src/components/Header/Header.component.js
+++ client/src/components/Header/Header.component.js
@@ -5,7 +5,7 @@ import './Header.styles.css';
 const Header = props => {
   return (
     <div className='headerContainer'>
-      <h3>Welcome to Room: {props.room}</h3>
+      <span className='titleHeader'>Welcome to Room: {props.room}</span>
       <div className='closeRoom'>
         <a href='/'>Close Room</a>
       </div>
