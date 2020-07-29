8ceae4849622851718fcd3a325961d09fafa3a5e
Shubhkirti Sharma <shubhkirti.work@gmail.com>
2020-07-29 19:38:23 +0530

    Add socket to get users in a room

--- server/index.js
+++ server/index.js
@@ -24,11 +24,13 @@ io.on('connection', (socket) => {
     socket.emit('message', { user: 'admin', text:`Hello ${user.name}. Welcome to Room: ${user.room}`});
     socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined the room`});
     socket.join(user.room);
+    io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
   })
 
   socket.on('sendMessage', (message, callback) => {
     const user = getUser(socket.id)
     io.to(user.room).emit('message', { user: user.name, text: message })
+    io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
     callback();
   })
 
