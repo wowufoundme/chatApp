c1477daa2a345ef61b20ccf36640e68c0989e729
Shubhkirti Sharma <shubhkirti.work@gmail.com>
2020-07-29 22:28:33 +0530

    Production build

---
+++ client/build/precache-manifest.1669dad600b848f5c5caff72411c3dce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ab31a93f228585b83fc5e687c255cd2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "438e1839bb08ca2cee65",
+    "url": "/static/css/main.249c5c46.chunk.css"
+  },
+  {
+    "revision": "2dd615e06c2e484de97a",
+    "url": "/static/js/2.ac52f9b9.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.ac52f9b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "438e1839bb08ca2cee65",
+    "url": "/static/js/main.1396f60f.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
